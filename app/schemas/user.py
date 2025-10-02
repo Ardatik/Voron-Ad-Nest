1f14d905ab89c5fda7d9028c477fe327721e743a
Ardatik <belogurovigor9@gmail.com>
2025-10-02 14:46:38 +0300

    add DataChange model

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, EmailStr, field_validator
+from pydantic import BaseModel, Field, EmailStr, field_validator, ConfigDict
 from uuid import UUID
 from typing import Optional
 from datetime import date
@@ -23,7 +23,7 @@ class UserRegister(BaseModel):
     )
     date_of_birth: date = Field(description="Дата рождения пользователя")
 
-    @field_validator("phone_number", mode="before")
+    @field_validator("phone_number")
     @classmethod
     def validate_phone_number(cls, phone: str) -> str:
         pattern = r"^\+?[1-9][0-9]{7,14}$"
@@ -31,13 +31,15 @@ class UserRegister(BaseModel):
             raise ValueError("Неверный формат номера телефона")
         return phone
 
-    @field_validator("date_of_birth", mode="before")
+    @field_validator("date_of_birth")
     @classmethod
     def validate_date_of_birth(cls, birth_date: date) -> date:
         if birth_date > date.today():
             raise ValueError("Неверно указана дата рождения")
         if (date.today() - birth_date).days / 365 < 14:
             raise ValueError("Возраст должен быть не менее 14 лет")
+        if (date.today() - birth_date).days / 365 > 100:
+            raise ValueError("указана корректуню дату рождения")
         return birth_date
 
 
@@ -49,10 +51,55 @@ class UserLogin(BaseModel):
     phone_number: Optional[str] = Field(
         default=None, description="телефон пользователя"
     )
-    password: str = Field(min_length=8, description="Пароль пользователя")
+    password: str = Field(
+        min_length=8, max_length=20, description="Пароль пользователя"
+    )
+
+
+class ChangeAccountData(BaseModel):
+    username: Optional[str] = Field(
+        default=None, min_length=6, max_length=16, description="username пользователя"
+    )
+    email: Optional[EmailStr] = Field(default=None, description="email пользователя")
+    first_name: Optional[str] = Field(
+        default=None, min_length=2, max_length=16, description="Имя пользователя"
+    )
+    last_name: Optional[str] = Field(
+        default=None, min_length=2, max_length=20, description="Фамилия пользователя"
+    )
+    patronymic_name: Optional[str] = Field(
+        default=None, min_length=2, max_length=20, description="Отчество пользователя"
+    )
+    date_of_birth: Optional[date] = Field(
+        default=None, description="Дата рождения пользователя"
+    )
+    password: Optional[str] = Field(
+        default=None, min_length=8, max_length=20, description="Пароль пользователя"
+    )
+
+    @field_validator("phone_number")
+    @classmethod
+    def validate_phone_number(cls, phone: str) -> str:
+        pattern = r"^\+?[1-9][0-9]{7,14}$"
+        if not re.match(pattern, phone):
+            raise ValueError("Неверный формат номера телефона")
+        return phone
+
+    @field_validator("date_of_birth")
+    @classmethod
+    def validate_date_of_birth(cls, birth_date: date) -> date:
+        if birth_date > date.today():
+            raise ValueError("Неверно указана дата рождения")
+        if (date.today() - birth_date).days / 365 < 14:
+            raise ValueError("Возраст должен быть не менее 14 лет")
+        if (date.today() - birth_date).days / 365 > 100:
+            raise ValueError("указана корректуню дату рождения")
+        return birth_date
 
 
 class UserResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: UUID
     username: str
     email: EmailStr
@@ -62,10 +109,3 @@ class UserResponse(BaseModel):
     patronymic_name: Optional[str]
     date_of_birth: date
     is_active: bool
-
-    class Config:
-        from_attributes = True
-
-
-class ChangeAccountData(BaseModel):
-    pass
