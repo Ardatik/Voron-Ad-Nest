120342abd2d14c1f951796afc4a1373edc4a75b7
Ardatik <belogurovigor9@gmail.com>
2025-10-06 18:00:03 +0300

    update db: add get_db function

--- app/db/database.py
+++ app/db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 from voron.app.models.base import Base
-from voron.app.models import account
+from voron.app.models import user
 from voron.app.models import advertisiment
 
 load_dotenv()
@@ -14,14 +14,21 @@ BD_HOST = os.getenv("BD_HOST")
 BD_PORT = os.getenv("BD_PORT")
 BD_NAME = os.getenv("BD_NAME")
 
-print(BD_USER, BD_PASSWORD, BD_HOST, BD_PORT, BD_NAME)
 
 SQLALCHEMY_DATABASE_URL = (
     f"postgresql+psycopg://{BD_USER}:{BD_PASSWORD}@{BD_HOST}:{BD_PORT}/{BD_NAME}"
 )
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
